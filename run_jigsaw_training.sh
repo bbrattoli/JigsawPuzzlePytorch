29a80cd5dec2812a894de11d636f9ee6b2d6650a
Biagio <biagio.brattoli@iwr.uni-heidelberg.de>
2018-02-16 14:44:15 +0100

    add run_jigsaw_training.sh

--- run_jigsaw_training.sh
+++ run_jigsaw_training.sh
@@ -1,5 +1,7 @@
 IMAGENET_FOLD=path_to_ILSVRC2012_img
-CHECKPOINTS_FOLD=path_to_output_folder
-GPU=0
 
-python JigsawTrain.py ${IMAGENET_FOLD} --checkpoint=${CHECKPOINTS_FOLD} --classes=1000 --lr=0.1 --gpu=${GPU}
+GPU=${1} # gpu used
+CHECKPOINTS_FOLD=${2} #path_to_output_folder
+
+python JigsawTrain.py ${IMAGENET_FOLD} --checkpoint=${CHECKPOINTS_FOLD} \
+                      --classes=1000 --batch 128 --lr=0.001 --gpu=${GPU} --cores=10
